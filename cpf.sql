ace852dd19d0726681fee4471d6f54bfe0bdd0f1
BRUNO <103595333+brunordmnc@users.noreply.github.com>
2022-09-02 17:19:35 -0300

    Add files via upload

---
+++ cpf.sql
@@ -0,0 +1,17 @@
+create database bdcpf;
+use bdcpf;
+
+create table cpf
+(
+id int unsigned not null auto_increment,
+cpf varchar(12)not null,
+cnpj varchar(11)not null,
+PRIMARY KEY (id)
+
+);
+
+
+
+
+
+
